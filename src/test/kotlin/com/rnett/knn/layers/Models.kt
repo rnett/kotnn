1f116d5e306324ad55cab690a425f7ef014c46be
Ryan Nett <rnett@calpoly.edu>
2019-03-29 23:42:49 -0700

    cleanup test

--- src/test/kotlin/com/rnett/knn/layers/Models.kt
+++ src/test/kotlin/com/rnett/knn/layers/Models.kt
@@ -25,14 +25,6 @@ import kotlin.system.measureTimeMillis
 
 class ConvTests {
 
-//    @Test
-//    fun `same diff tests`() {
-//        val sd = SameDiff.create()
-//
-//        val a = sd.linspace(0, 10, 10)
-//
-//    }
-
     @Test
     fun `test mnist capsnet`() {
 
@@ -44,11 +36,11 @@ class ConvTests {
         deviceNativeOps.setOmpNumThreads(6)
 
         val model = graph {
-            val image = input("image", "depth" to 1, "height" to 28, "depth" to 28)
+            val image = input("image", 1, 28, 28)
 
             optimizer = Optimizers.Adam
 
-            image.reshape(1, 28, 28)
+            image.reshape(1, 28, 28) // the input is flat, reshape it
 
             println(image.shape)
 
@@ -60,15 +52,6 @@ class ConvTests {
                 +CapsuleLayer(10, 16, 3)
                 println("Caps: ${image.shape}")
                 +CapsuleStrength()
-//                +Convolution2DLayer(20, 5.p2) { activation = ActivationLReLU() }
-//                +Subsampling2DLayer(kernelSize = 2.p2, stride = 2.p2)
-//
-//                +Convolution2DLayer(20, 5.p2) { activation = ActivationLReLU() }
-//                +Subsampling2DLayer(kernelSize = 2.p2, stride = 2.p2)
-//
-                //flatten()
-
-                //+DenseLayer(10) { activation = ActivationLReLU() }
 
                 +ActivationLayer(Activations.Softmax)
 
@@ -109,7 +92,7 @@ class ConvTests {
     fun `test mnist`() {
 
         val model = graph {
-            val image = input("image", "depth" to 1, "height" to 28, "depth" to 28)
+            val image = input("image", 1, 28, 28)
 
             image.reshape(1, 28, 28)
 
@@ -156,62 +139,5 @@ class ConvTests {
         println(eval.stats())
 
     }
-
-//    @Test
-//    fun `test mnist java`() {
-//
-//        val conf = NeuralNetConfiguration.Builder()
-//            .updater(Updater.SGD.iUpdaterWithDefaultConfig)
-//            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-//            .list()
-//            .layer(0, ConvolutionLayer.Builder()
-//                .kernelSize(5, 5).nOut(20).activation(ActivationLReLU())
-//                .build())
-//            .layer(1, SubsamplingLayer.Builder()
-//                .kernelSize(2, 2).stride(2 ,2)
-//                .build())
-//            .layer(2, ConvolutionLayer.Builder()
-//                .kernelSize(5, 5).nOut(20).activation(ActivationLReLU())
-//                .build())
-//            .layer(3, SubsamplingLayer.Builder()
-//                .kernelSize(2, 2).stride(2 ,2)
-//                .build())
-//            .layer(4, org.deeplearning4j.nn.conf.layers.DenseLayer.Builder()
-//                .nOut(200).activation(ActivationLReLU())
-//                .build())
-//            .layer(5, org.deeplearning4j.nn.conf.layers.OutputLayer.Builder()
-//                .nOut(10).activation(ActivationSoftmax()).lossFunction(LossNegativeLogLikelihood())
-//                .build())
-//            .setInputType(InputType.convolutionalFlat(28, 28, 1)).build()
-//
-//        val model = MultiLayerNetwork(conf)
-//
-//        //model.init()
-//
-//        val rngSeed = 12345
-//        val mnistTrain = MnistDataSetIterator(64, true, rngSeed)
-//        val mnistTest = MnistDataSetIterator(64, false, rngSeed)
-//
-//
-//        val nEpochs = 5
-//
-//        (1..nEpochs).forEach { epoch ->
-//            val time = measureTimeMillis {
-//                try {
-//
-//                    model.fit(mnistTrain)
-//                } catch (e: Exception) {
-//                    e.printStackTrace()
-//                }
-//            }
-//
-//            println("Epoch " + epoch + " complete, took ${time / 1000} seconds")
-//        }
-//        println(model.summary())
-//
-//        val eval: Evaluation = model.evaluate(mnistTest)
-//        println(eval.stats())
-//
-//    }
 
 }
